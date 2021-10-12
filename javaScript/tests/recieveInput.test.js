7059e2f82456d8532098619b50b48c35cb1bbdb2
Harrisonp365 <harrisonpowell365@gmail.com>
2021-10-12 17:26:28 +1100

    setting up and first TDD test almost written out along with some styling I am trying to work on and make better

---
+++ javaScript/tests/recieveInput.test.js
@@ -0,0 +1,28 @@
+import { it, test } from "@jest/globals"
+import { describe } from "yargs"
+import { recieveInput } from "../modules/recieveInput.js";
+
+describe("Checks input is correct and also returns the correct output"), () => {
+    const paramTypeError = new Error("Must enter morse code or english");
+    it("Should only take in params which are in the alphabet"), () => { 
+        expect(() => recieveInput({})).toThrowError(paramTypeError);
+    };
+
+    it("Should return Morse code if english is input"), () => {
+        expect(recieveInput("d")).toBe("-..");
+        expect(recieveInput("o")).toBe("---");
+    };
+
+    it("Should return English if morse code is input"), () => {
+        expect(recieveInput("-..")).toBe("d");
+        expect(recieveInput("---")).toBe("o");
+    };
+
+    it("Should return an alert if nothing is input"), () => {
+        expect(() => recieveInput({})).toThrowError(paramTypeError);
+    };
+
+    it("Should only accept lowerCase characters"),() => {
+
+    };
+}
